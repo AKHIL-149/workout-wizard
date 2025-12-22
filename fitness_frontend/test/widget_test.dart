da3d132d5df982db04a1f8117390ff76d40ad09f
AKHIL-149 <venkatakhil149@gmail.com>
2025-12-22 16:00:15 -0600

    Fix widget_test.dart skip parameter syntax

--- fitness_frontend/test/widget_test.dart
+++ fitness_frontend/test/widget_test.dart
@@ -5,11 +5,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:fitness_frontend/main.dart';
 
 void main() {
+  // TODO: Re-enable after adding mocking infrastructure for SplashScreen timer and services
   testWidgets('App builds and shows MaterialApp', (WidgetTester tester) async {
     // Build our app and trigger a frame
     await tester.pumpWidget(const FitnessRecommenderApp());
 
     // Verify that our app builds successfully
     expect(find.byType(MaterialApp), findsOneWidget);
-  }, skip: 'Skipping: SplashScreen timer and service initialization require mocking infrastructure');
+  }, skip: true);
 }
